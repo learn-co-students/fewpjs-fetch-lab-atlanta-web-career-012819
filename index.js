d48ed013bbf457d1fc679feafe931ad0fd071df4
Dana Simmons <dana@simmons-audio.com>
2019-03-12 18:22:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then(data => console.log(data))
+
+
+  // .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -17,3 +20,23 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 });
+
+// fetch('https://anapioficeandfire.com/api/books', 
+//   {
+//     mode:'no-cors'
+//   })
+//   .then(function(resp){
+//     return resp.json()})
+//   .then(data => console.log("MY DATA", data))
+
+// fetch('https://anapioficeandfire.com/api/books',
+// {
+//   method: 'GET',
+//   mode: 'no-cors'
+// })
+//   .then(function(response) {
+//     return response.json()
+//   })
+//   .then(function(myJson) {
+//     console.log(JSON.stringify(myJson))
+//   });
